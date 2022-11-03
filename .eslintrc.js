01583289cb21448a4964537a4ed5d10c7df2258e
Matheus Toniolli <matheus.toniolli@hotmail.com>
2022-11-03 18:14:48 +0000

    chore: eslint standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-  extends: '@loopback/eslint-config',
+  extends: [
+    '@loopback/eslint-config',
+    "standard",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "standard-with-typescript"
+  ],
+  "rules": {
+    // Note: you must disable the base rule as it can report incorrect errors
+  }
 };
